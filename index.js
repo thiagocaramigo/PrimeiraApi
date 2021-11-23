b2d48f35c8320ad0c1de4c5ca3ce3f7db1d18cf1
Thiago Américo Caramigo Marciano do Prado <thico.caramigo@gmail.com>
2021-11-23 11:57:19 -0300

    atualização do projeto primeira api

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const express = require("express");
 // nodejs e o banco de dados mongodb
 const mongoose = require("mongoose");
 
+// Importação do modulo bycrpt para criptografia de senhas
+const bycrpt = require("bcrypt");
+
 const url = "mongodb+srv://thiagoprado:vaicurintia1910@clustername.fpofj.mongodb.net/primeiraapi?retryWrites=true&w=majority";
 
 mongoose.connect(url, {useNewUrlParser:true, useUnifiedTopology:true });
@@ -23,6 +26,20 @@ const tabela = mongoose.Schema({
 
 
 });
+
+// Aplicação da criptografia do bcrypt a tabela de cadastro
+// de clientes será feita um passo antes do salvamento
+// dos dados cliente
+// vamos usar o comando pre
+tabela.pre("save",function(next){
+    let cliente = this;
+    if(!cliente.isModified('senha')) return next()
+    bycrpt.hash(cliente.senha,10,(erro,rs)=>{
+        if(erro) return console.log(`erro ao gerar senha ->${erro}`);
+        cliente.senha = rs;
+        return next();
+    }) 
+})
 
 // execução de tabela
 const Cliente = mongoose.model("tbcliente",tabela);
@@ -62,6 +79,22 @@ app.get("/api/cliente/",(req,res)=>{
     );
 });
 
+
+
+
+app.get("/api/cliente/:id",(req,res)=>{
+    Cliente.findById(req.params.id,(erro,dados)=>{
+        if(erro){
+            return res.status(400).send({output:`Erro ao tentar ler os clientes -> ${erro}`});
+        }
+        res.status(200).send({output:dados});
+    }
+
+    );
+});
+
+
+
 app.post("/api/cliente/cadastro",(req,res)=>{
 
     const cliente = new Cliente(req.body);
@@ -71,6 +104,23 @@ app.post("/api/cliente/cadastro",(req,res)=>{
     .catch((erro)=>res.status(400).send({output:`Erro ao tentar cadastradar o cliente -> ${erro}`}))
 
 });
+
+app.post("/api/cliente/login",(req,res)=>{
+    const us = req.body.usuario;
+    const sh = req.body.senha;
+    Cliente.findOne({usuario:us},(erro,dados)=>{
+        if(erro){
+            return res.status(400).send({output:`Usuário não localizado->${erro}`});
+        }
+        bycrpt.compare(sh,dados.senha,(erro,igual)=>{
+            if(erro) return res.status(400).send({output:`Erro ao tentar logar->${erro}`});
+            if(!igual) return res.status(400).send({output:`Erro ao tentar logar->${erro}`});
+            res.status(200).send({output:`Logado`,playload:dados});
+        })
+    });
+});
+
+
 
 
 app.put("/api/cliente/atualizar/:id",(req,res)=>{
